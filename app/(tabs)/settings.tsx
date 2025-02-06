3dc2dc6fbe0ab7cc42c41b4ce41c16e8ce7d1f39
Fazle Rabbi <24.fazlerabbi@gmail.com>
2025-02-07 02:12:26 +0600

    inital commit

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,24 +1,29 @@
 import { StyleSheet } from 'react-native';
+
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { View } from 'react-native';
 
-export default function HomeScreen() {
+export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
+  
       <ThemedView style={styles.container}>
-        <ThemedText type="title">Welcome!</ThemedText>
-      </ThemedView>
-     
+        <ThemedText type="title">Settings</ThemedText>
+    </ThemedView>
     </View>
   );
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    justifyContent: 'center',
+   justifyContent: 'center',
     alignItems: 'center',
+    padding: 16,
   }
 });
+     
